f0ca6226d4d1ee51a6ac74cfada5db6a360d00d3
einaooka <eooka@teainc.org>
2016-01-26 14:35:53 -0800

    For smooth disaggregation of monthly st into daily ts, denton method is implemented.

--- R/DailyAndMonthly.r
+++ R/DailyAndMonthly.r
@@ -2,11 +2,14 @@
 #' @title Convert between Daily and Monthly Data Frames
 #'
 #' @description ConvertDailyToMonthly takes a daily table and convert it to a monthly table with values equal to the mean.
-#' Conversely, ConvertMonthlyToDaily takes a monthly table and convert it to a daily table, either as a step function or
-#' pricewise-defined 2nd-degree polynomials.
+#' Conversely, ConvertMonthlyToDaily takes a monthly table and convert it to a daily table.
 #'
 #' @param df A data frame to be converted. The first column needs to be a date column.
-#' @param type Smoothing type for monthly-to-daily conversion. c("step", "smooth"). Default set at "step."
+#' @param type Smoothing type for monthly-to-daily conversion. c("step", "smooth", "smooth.naive). Default set at "step."
+#' 
+#' @details When 'step' is chosen for disaggregation method, data is simply extrapolated as constants through the months.
+#' For 'smooth' disaggregation, the default setting is by denton method. If naive smoothing is chosen, piece-wise deinfed 2nd degree 
+#' polynomial method is used. 
 #'
 #' @import tea.datetime
 #'
@@ -14,6 +17,8 @@
 #' @export ConvertMonthlyToDaily
 #'
 #' @examples
+#' library(reshape)
+#' library(tea.eo.plots)
 #' niter <- 2
 #' monthly.df <- data.frame("Date"=rep(seq(as.Date("2010-1-1"), by="month", length.out = 12),niter)
 #' , "Iter" = rep(1:niter,each=12), "Y"=rnorm(12*niter))
@@ -52,24 +57,52 @@ ConvertMonthlyToDaily <- function(df, type="step"){
   idx <- match(tea.datetime::FirstDayOfMonth(return.df$Date), df[,1])
   return.df <- cbind(return.df, df[idx,-1])
 
-  # Smooth -- 2nd degree polynomial
-  if (type == "smooth"){
-
+  if (type == "smooth"){ # denton temporal disaggregation
+    
+    # number of steps
+    n_l <- nrow(df)
+    n <- nrow(return.df)
+    
+    # preliminary series
+    p <- rep(1, n)
+    
+    # variance-covariance matrix for addtive Denton
+    D <- diag(n)
+    diag(D[2:n, 1:(n - 1)]) <- -1
+    Q <- solve(t(D) %*% D)
+    
+    # Conversion matrix
+    C <- matrix(0, nrow=n_l, ncol=n)
+    idx <- 0
+    for (i in 1:nrow(df)){
+      num.days <- NumDaysInMonth(df$Date[i])
+      C[i, idx+c(1:num.days)] <- 1/num.days
+      idx <- idx + num.days
+    }
+    
+    # Distribution matrix
+    D <- Q %*% t(C) %*% solve(C %*% Q %*% t(C))
+    
+    # Reconcile results
+    for (i in 2:nvars) return.df[,i] <- p + D %*% (df[,i] - C %*% p) 
+    
+  } else if (type == "smooth.naive"){
+    
     day.idx <- as.numeric(format(return.df$Date, "%d"))/tea.datetime::NumDaysInMonth(return.df$Date)
-
+    
     temp <- glag(df[,-1],1)
     temp[1,] <- df[1,-1] # Fill the first row
     prev <- (return.df[,-1] + temp[idx,])/2
-
+    
     temp <- glag(df[,-1],-1)
     temp[nrow(temp),] <- df[nrow(temp),-1] # Fill the end row
     after <- (return.df[,-1] + temp[idx,])/2
-
+    
     # polynomial coefficients
     a <- prev
     b <- 6 * return.df[,-1] - 4 * prev - 2 * after
     c <- 3 * after + 3 * prev - 6 * return.df[,-1]
-
+    
     return.df[,-1] <- a + apply(b, 2, "*", day.idx) + apply(c, 2, "*", (day.idx^2))
   }
   return(return.df[,1:nvars])
